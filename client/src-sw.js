91b26d7f26471e00862e6a18dc1fc2830ef44e6d
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-08-04 21:21:50 -0500

    re-organized code to make it more organized

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,39 +1,39 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+// Imports
+const { 
+  offlineFallback, 
+  warmStrategyCache 
+} = require("workbox-recipes");
 const { CacheFirst } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Pre-cache routes
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: "page-cache",
+// Utility function to create a CacheFirst instance with customizable options
+const createCacheFirstStrategy = (cacheName, maxAgeSeconds) => new CacheFirst({
+  cacheName,
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+    maxAgeSeconds ? new ExpirationPlugin({ maxAgeSeconds }) : null,
+  ].filter(Boolean),
 });
 
+// Page caching
+const pageCache = createCacheFirstStrategy("page-cache", 30 * 24 * 60 * 60);
 warmStrategyCache({
   urls: ["/index.html", "/"],
   strategy: pageCache,
 });
-
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
+// Asset caching
+const assetCache = createCacheFirstStrategy("asset-cache");
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new CacheFirst({
-    cacheName: "asset-cache",
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
+  assetCache
 );
