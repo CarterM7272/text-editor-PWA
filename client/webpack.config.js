d8427b947232ef6d72e7dcc9c92a62db8f50531a
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-08-04 21:24:01 -0500

    made code more readable and organized

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,65 +3,72 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
+// Define paths
+const PATHS = {
+  src: path.resolve(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'dist'),
+  assets: path.resolve(__dirname, 'src/assets'),
+};
 
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "index.html",
-        title: "Jate",
-      }),
-
-      new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "sw.js",
-      }),
-
-      new WebpackPwaManifest({
-        name: "Jate",
-        short_name: "Jate",
-        description: "Jate is a simple note taking app",
-        background_color: "#ffffff",
-        theme_color: "#2196f3",
-        crossorigin: "use-credentials",
-        icons: [
-          {
-            src: path.resolve("src/assets/icon.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          },
-        ],
-      }),
+// Plugins configuration
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+    title: 'Jate',
+  }),
+  new InjectManifest({
+    swSrc: path.join(PATHS.src, 'src-sw.js'),
+    swDest: 'sw.js',
+  }),
+  new WebpackPwaManifest({
+    name: 'Jate',
+    short_name: 'Jate',
+    description: 'Jate is a simple note taking app',
+    background_color: '#ffffff',
+    theme_color: '#2196f3',
+    crossorigin: 'use-credentials',
+    icons: [
+      {
+        src: path.resolve(PATHS.assets, 'icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons'),
+      },
     ],
+  }),
+];
 
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              plugins: ["@babel/plugin-transform-runtime"],
-              presets: ["@babel/preset-env"],
-            },
-          },
-        },
-      ],
+// Module rules configuration
+const rules = [
+  {
+    test: /\.css$/i,
+    use: ['style-loader', 'css-loader'],
+  },
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        plugins: ['@babel/plugin-transform-runtime'],
+        presets: ['@babel/preset-env'],
+      },
     },
-  };
+  },
+];
+
+// Webpack configuration
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.join(PATHS.src, 'js/index.js'),
+    install: path.join(PATHS.src, 'js/install.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: PATHS.dist,
+  },
+  plugins,
+  module: {
+    rules,
+  },
 };
